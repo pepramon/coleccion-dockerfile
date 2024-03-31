eebbb3c3b1fe1d0c9d0bb66ee1413aba28938979
pepramon <pepramon@gmail.com>
2024-03-31 18:59:16 +0200

    Correción errores a saco

--- construir-y-subir-imagenes.sh
+++ construir-y-subir-imagenes.sh
@@ -4,10 +4,10 @@ usuario=pepramon
 
 logeado=`jq '.auths | length' ~/.docker/config.json`
 
-[ $docker_login_info -eq 0 ] && echo '\n\n No estas logueado en Docker Hub!!!!!\ 
-no se subiran las imagenes automaticamente \n\n' || 
+echo '\n\n ATENCIÓN --> Si no estas logueado en Docker Hub,'\ 
+'no se subiran las imagenes automaticamente \n\n'
 
 
 ## Simple-samba, consturcción y push
 docker build -t $usuario/simple-samba ./simple-samba/.
-[ $docker_login_info -ne 0 ] && docker push $usuario/simple-samba ||
+docker push $usuario/simple-samba
