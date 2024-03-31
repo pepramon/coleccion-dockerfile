e92e2310af672df69d84e1d535e097df7238519e
pepramon <pepramon@gmail.com>
2024-03-31 19:17:35 +0200

    Se añade simple-samba y esqueleto del poyecto

---
+++ construir-y-subir-imagenes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+usuario=pepramon
+
+echo -e "\n\nATENCIÓN --> Si no estas logueado en Docker Hub,\nno se subiran las imagenes automaticamente \n\n"
+sleep 1
+
+## Simple-samba, consturcción y push
+docker build -t $usuario/simple-samba ./simple-samba/.
+docker push $usuario/simple-samba
